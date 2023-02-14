c94f9275d5c292f637f534ff5db684096123b415
hiroki.nakamura <125346034+hiroki-coffee@users.noreply.github.com>
2023-02-14 23:49:03 +0900

    Chapter5

---
+++ src/Chapter05.java
@@ -0,0 +1,16 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String str1;
+		str1 ="hiroki♪";
+		System.out.println(str1);
+		str1 ="捲土重来";
+				System.out.println(str1);
+				final int luckynumber = 777;
+	    System.out.println(luckynumber);
+		
+	}
+
+}
